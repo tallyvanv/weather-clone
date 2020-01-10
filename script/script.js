bccb14f1a5cb6e0af019dad77c587acc3191e3fe
tallyvanv <talisa.vdv@gmail.com>
2020-01-10 15:55:50 +0100

    got the object in my console

---
+++ script/script.js
@@ -0,0 +1,14 @@
+(() => {
+
+    async function getWeather() {
+        let city = document.getElementById("weatherLocation").value;
+        const apiKey = "appid=239a3fc05faa415afe41364be9e3c3f7";
+        let path = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&${apiKey}&units=metric`;
+
+        const weather = await fetch(path);
+        const data = await weather.json();
+        console.log(data);
+    }
+
+    document.getElementById("run").onclick = function() {getWeather()};
+})();
