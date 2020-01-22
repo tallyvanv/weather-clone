79975d5b182a85891be36806c964dd4805797daf
tallyvanv <talisa.vdv@gmail.com>
2020-01-22 19:46:33 +0000

    test

--- script/script.js
+++ script/script.js
@@ -25,7 +25,7 @@
 
 
         const apiKey = "appid=239a3fc05faa415afe41364be9e3c3f7";
-        let path = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&${apiKey}&units=metric`;
+        let path = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&${apiKey}&units=metric`;
 
         const weather = await fetch(path);
         const data = await weather.json();
